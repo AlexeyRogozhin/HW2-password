0d4e9ddb787a4708fe14477ffd290466f6b9f865
AlexeyRogozhin <126471630+AlexeyRogozhin@users.noreply.github.com>
2023-03-01 16:30:43 +0100

    password

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);//создала объект
+        String truePass = "password123";
+
+        while (true) {
+            System.out.print("Введите пароль : ");
+            String str = scanner.nextLine();
+
+            if (str.equals(truePass)) {
+                System.out.println("Вы ввели верный пароль!");
+                break;
+            } else if ("exit".equals(str)) {
+                System.out.println("Вы вышли из системы!");
+                break;
+            }
+            System.out.println("Вы ввели : " + str);
+            System.out.println("Пароль не верный. Попробуйте ещё раз или введите 'exit'");
+        }
+        scanner.close();
+    }
+}
